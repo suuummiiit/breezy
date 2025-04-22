2b738e9c905079ea69946a217181969a4d79f2b9
sumit <suuummiiit@gmail.com>
2025-04-23 03:46:08 +0530

    adding csrf trusted domains

--- breezy/settings.py
+++ breezy/settings.py
@@ -27,6 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['breezy-kw8e.onrender.com', 'localhost', '127.0.0.1']
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://breezy-kw8e.onrender.com"
+]
+
 
 
 # Application definition
